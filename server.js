1e0da77ed5063e483fae16e9e6d32c9b6bf3d2c5
Emmanuel6093 <elorenzo6093@gmail.com>
2022-04-26 20:19:07 -0400

    saving data to db.json

--- server.js
+++ server.js
@@ -29,10 +29,29 @@ app.get('/api/notes', function (req, res) {
     res.sendFile(path.join(__dirname, '/db/db.json'))
 }); 
 
+// save notes with id 
 app.get('api/notes:id', function (req, res) {
-    let SavedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'));
+    let SavedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     res.json(SavedNotes[Number(req.params.id)]);
 });
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(mainDir, 'index.html'))
+});
+
+app.post('/api/notes', function (req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let newNote = req.body; 
+    let uniqueID = uniqueID;
+
+    savedNotes.push(newNote); 
+
+    fs.writeFile('./db/db.json', JSON.stringify(savedNotes));
+
+    console.log("Note saved to db.json. Content:", newNote);
+
+    res.json(savedNotes); 
+})
 
 app.listen(PORT, e => console.log('Running'))
 
